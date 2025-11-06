6c67db4215483fcba5fb2d8be3849656a665069a
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-06 14:12:15 +0900

    Preprocessing data random_forest02.py

--- 4주차 실습/DecisionTree/random_forest02.py
+++ 4주차 실습/DecisionTree/random_forest02.py
@@ -7,8 +7,27 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import platform
+if platform.system() == 'Windows':
+    plt.rc('font', family='Malgun Gothic')
+plt.rcParams['axes.unicode_minus'] = False
+
 # 1. 데이터 로드
 penguins = sns.load_dataset('penguins')
 
 # 2. 데이터 전처리
+# 수치형/범주형 변수 분리
+num_cols = penguins.select_dtypes(include='number') # 'bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'
+cat_cols = penguins.select_dtypes(exclude='number') # 'species', 'island', 'sex'
 
+# 결측치 처리(수치형:중앙값, 범주형:최빈값)
+# print(num_cols.isnull().sum())
+# print(cat_cols.isnull().sum())
+for n in num_cols.columns:
+    penguins[n] = penguins[n].fillna(penguins[n].median())
+for c in cat_cols.columns:
+    penguins[c] = penguins[c].fillna(penguins[c].mode()[0])
+# print(penguins.isnull().sum())
+
+# 범주형 원핫인코딩
+x = pd.get_dummies(penguins, columns=cat_cols, drop_first=True).drop('species', axis=1)
