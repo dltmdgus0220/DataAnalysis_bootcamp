fc690031442c5af99b0fe5d9517677475e1ab054
dltmdgus0220 <dltmdgus@khu.ac.kr>
2025-11-10 15:20:00 +0900

    feat : 다항회귀모델 추가

--- 4주차 실습/LinearRegression/linear_regression01.py
+++ 4주차 실습/LinearRegression/linear_regression01.py
@@ -54,3 +54,36 @@ yB_pred, yB_residual, yB_model = fit_and_plot_residual(x, yB, '부분 위배(선
 
 print(f'Case B - Linear model R^2 : {r2_score(yB, yB_pred):.4f}')
 # r2_score : 회귀모델의 성능(설명력)을 평가할 때 사용하는 가장 기본적인 평가지표
+
+
+# 다항회귀모델
+poly_model = Pipeline(steps=[
+    ('poly', PolynomialFeatures(degree=2, include_bias=False)), # 2차항 생성, degree가 n이면 n차항까지 생성
+    ('lin', LinearRegression()) # LinearRegression 모델에는 절편을 포함해서 학습하기 때문에 위 코드에서 include_bias를 False로 설정
+])
+
+X_poly = x.reshape(-1, 1)
+poly_model.fit(X_poly, yB)
+yB_poly_pred = poly_model.predict(X_poly)
+yB_poly_residual = yB - yB_poly_pred
+
+plt.figure(figsize=(6,4))
+plt.scatter(x, yB, alpha=0.7)
+order = np.argsort(x) # 정렬인덱스 저장
+plt.plot(x[order], yB_poly_pred[order]) # 선형회귀식에 의한 1차직선
+plt.title(f"y vs x (+Linear Fit)")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.tight_layout()
+plt.show()
+
+
+plt.figure(figsize=(6,4))
+plt.scatter(yB_poly_pred, yB_poly_residual, alpha=0.7) # 예측값에 따른 잔차를 산점도로 확인
+plt.axhline(0, linestyle='--') # 수평선 긋기, 양수음수를 구분하기 위해 y축 생성, 즉 잔차가 0인 기준선 긋기
+plt.title(f'Residual VS Fitted')
+plt.xlabel('Predicted (Fitted)')
+plt.ylabel('Residuals (y-y_pred)')
+plt.tight_layout()
+plt.show()
+print(f'Case Poly B - Linear model R^2 : {r2_score(yB, yB_poly_pred):.4f}')
