4db27a04d0ffd4046ee5e00897c0a9e63239a277
dltmdgus0220 <dltmdgus@khu.ac.kr>
2025-11-10 17:45:54 +0900

    feat : mpg 데이터 베이스모델 학습 및 잔차시각화

--- 4주차 실습/LinearRegression/linear_regression04.py
+++ 4주차 실습/LinearRegression/linear_regression04.py
@@ -15,6 +15,52 @@ if platform.system() == "Windows":
     plt.rc('font', family="Malgun Gothic")
 plt.rcParams['axes.unicode_minus']=False
 
-df = sns.load_dataset('mpg', as_frame=True)
-X = df.drop(columns=['mpg','name'])
+df = sns.load_dataset('mpg')
+X = df.drop(columns=['mpg','name','horsepower'])
 y = df['mpg']
+# print(df.isnull().sum())
+
+num_cols = X.select_dtypes(include=['float','int']).columns.to_list()
+cat_cols = X.select_dtypes(include=['category','object']).columns.to_list()
+
+print(f'num_cols : {num_cols}')
+print(f'cat_cols : {cat_cols}')
+
+preprocess = ColumnTransformer(
+    transformers=[
+        ('num', 'passthrough', num_cols),
+        ('cat', OneHotEncoder(handle_unknown='ignore'), cat_cols)
+    ],
+    remainder='drop'
+)
+
+model = Pipeline(steps=[
+    ('prep', preprocess),
+    ('reg', LinearRegression())
+])
+
+x_tr, x_te, y_tr, y_te = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model.fit(x_tr, y_tr)
+pred = model.predict(x_te)
+
+r2 = r2_score(y_te, pred)
+mae = mean_absolute_error(y_te, pred)
+mse = mean_squared_error(y_te, pred)
+rmse = np.sqrt(mse)
+print(f'R2 : {r2}')
+print(f'MAE : {mae}')
+print(f'MSE : {mse}')
+print(f'RMSE : {rmse}')
+
+resid = y_te - pred
+
+# fig, axes = plt.subplots(1,3, figsize=(14,4))
+plt.figure(figsize=(6,4))
+plt.scatter(pred, resid, alpha=0.7)
+plt.axhline(0, linestyle='--')
+plt.xlabel('Predicted')
+plt.ylabel('Residual')
+plt.title('Residual vs Predicted')
+plt.tight_layout()
+plt.show()
