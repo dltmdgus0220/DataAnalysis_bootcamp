9524518dcf0af789354a9f7116d41294bf6cc62b
dltmdgus0220 <dltmdgus@khu.ac.kr>
2025-11-10 14:29:57 +0900

    feat : 잔차 시각화 함수 생성

--- 4주차 실습/LinearRegression/linear_regression01.py
+++ 4주차 실습/LinearRegression/linear_regression01.py
@@ -4,3 +4,38 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import r2_score
+
+rng = np.random.default_rng(42)
+n = 120
+
+def fit_and_plot_residual(x, y, title, save_prefix=None): # save_prefix는 저장이 필요할 때 사용할 경로
+    X = x.reshape(-1,1) # 다행1열로 reshape, x:1차원 X:2차원
+    model = LinearRegression().fit(X, y) # 회귀식 생성, linearregression은 무조건 2d 형태여야함.
+    y_pred = model.predict(X) # 오차를 계산하기 위해 X로 다시 예측
+    residual = y - y_pred
+
+    plt.figure(figsize=(6,4))
+    plt.scatter(X, y, alpha=0.7)
+    order = np.argsort(X) # 정렬인덱스 저장
+    plt.plot(X[order], y_pred[order]) # 선형회귀식에 의한 1차직선
+    plt.title(f"y vs x (+Linear Fit) - {title}")
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.tight_layout()
+    if save_prefix:
+        plt.savefig(f'{save_prefix}_scatter_fit.png')
+    plt.show()
+
+    # 잔차 시각화
+    plt.figure(figsize=(6,4))
+    plt.scatter(y_pred, residual, alpha=0.7) # 예측값에 따른 잔차를 산점도로 확인
+    plt.axhline(0, linestyle='--') # 수평선 긋기, 양수음수를 구분하기 위해 y축 생성, 즉 잔차가 0인 기준선 긋기
+    plt.title(f'Residual VS Fitted - {title}')
+    plt.xlabel('Predicted (Fitted)')
+    plt.ylabel('Residuals (y-y_pred)')
+    plt.tight_layout()
+    if save_prefix:
+        plt.savefig(f'{save_prefix}_residual.png')
+    plt.show()
+
+    return y_pred, residual, model
