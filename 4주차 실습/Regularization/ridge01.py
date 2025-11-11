97d7aae7c46fc8aa62a2ceb89e9cdd7ec62050a1
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-11 15:23:28 +0900

    feat : drop_high_corr_features 함수 추가

--- 4주차 실습/Regularization/ridge01.py
+++ 4주차 실습/Regularization/ridge01.py
@@ -41,3 +41,34 @@ def compute_vif(df_num:pd.DataFrame) -> pd.DataFrame:
     for i in range(1, Z.shape[1]):
         vifs.append(variance_inflation_factor(Z, i))
     return pd.DataFrame( { "feature" : df_num.columns, "VIF" : vifs } ).sort_values('VIF', ascending=False)
+
+vif_before = compute_vif(X.astype(float))
+print(vif_before) 
+# displacement  19.641683
+# weight  10.731681
+# cylinders  10.633049
+# horsepower   9.398043
+# acceleration   2.625581
+# model_year   1.244829
+
+
+def drop_high_corr_features(df_num:pd.DataFrame, thr:float=0.9) -> list:
+    cols = df_num.columns.tolist()
+
+    while True:
+        C = df_num[cols].corr(numeric_only=True).abs()
+        np.fill_diagonal(C.values, 0) # 대각선은 자기자신과의 상관관계를 뜻하므로 0으로 바꾸기
+        max_corr = C.values.max()
+        if max_corr < thr or len(cols) <= 1: # 임계값보다 높은 상관계수가 없거나 남은 컬럼이 없다면 종료
+            break
+
+        i, j = np.where(C.values == max_corr) # (2,3) (3,4) => i=[2,3], j=[3,4] 이렇게 들어감
+        i0, j0 = int(i[0]), int(j[0])
+        col_i, col_j = cols[i0], cols[j0] # 가장 큰 상관계수를 가지는 피처들을 추출
+        mean_i = C.loc[col_i, :].mean()
+        mean_j = C.loc[col_j, :].mean()
+
+        drop_col = col_i if mean_i >= mean_j else col_j # 다른 피처들과의 상관계수의 평균이 더 큰 피처를 드랍하기 위함
+        cols.remove(drop_col)
+
+    return cols
