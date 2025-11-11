46117bd8738d21f54868060c605ac32d1d1e912f
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-11 16:34:06 +0900

    feat : 선형성 검증 (horsepower, weight)

--- 4주차 실습/LinearRegression/linear_regression06.py
+++ 4주차 실습/LinearRegression/linear_regression06.py
@@ -31,3 +31,36 @@ num_cols = df.select_dtypes(include=['int', 'float']).columns.tolist()
 X = df[num_cols].copy()
 y = df['mpg'].copy()
 
+#===============================================
+# 2. 선형성 검증을 위한 시각화 (horsepower, weight)
+#===============================================
+def fit_and_plot_residual(x, y):
+    X = x.reshape(-1,1) # 다행1열로 reshape, x:1차원 X:2차원
+    model = LinearRegression().fit(X, y) # 회귀식 생성, linearregression은 무조건 2d 형태여야함.
+    y_pred = model.predict(X) # 오차를 계산하기 위해 X로 다시 예측
+    resid = y - y_pred
+
+    plt.figure(figsize=(6,4))
+    plt.scatter(x, y, alpha=0.7)
+    order = np.argsort(x) # 정렬인덱스 저장
+    plt.plot(x[order], y_pred[order]) # 선형회귀식에 의한 1차직선
+    plt.title("y vs x (+Linear Fit)")
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.tight_layout()
+    plt.show()
+
+    # 잔차 시각화 : 패턴이 있으면 안됨. 패턴이 있으면 모델이 데이터 구조를 제대로 설명하지 못했다는 신호.
+    plt.figure(figsize=(6,4))
+    plt.scatter(y_pred, resid, alpha=0.7) # 예측값에 따른 잔차를 산점도로 확인
+    plt.axhline(0, linestyle='--') # 수평선 긋기, 양수음수를 구분하기 위해 y축 생성, 즉 잔차가 0인 기준선 긋기
+    plt.title(f'Residual VS Fitted')
+    plt.xlabel('Predicted (Fitted)')
+    plt.ylabel('Residuals (y-y_pred)')
+    plt.tight_layout()
+    plt.show()
+
+    return y_pred, resid, model
+
+fit_and_plot_residual(X['horsepower'].to_numpy(), y)
+fit_and_plot_residual(X['weight'].to_numpy(), y)
