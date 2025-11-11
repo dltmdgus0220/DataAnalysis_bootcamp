cf69d1acd227e785dabd885f6d794aa85f9c1a9b
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-11 16:20:59 +0900

    Create linear_regression06.py

---
+++ 4주차 실습/LinearRegression/linear_regression06.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, PolynomialFeatures
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from scipy import stats
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+import platform
+if platform.system() == "Windows":
+    plt.rc('font', family="Malgun Gothic")
+plt.rcParams['axes.unicode_minus']=False
