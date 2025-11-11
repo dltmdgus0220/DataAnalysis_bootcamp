1634c1d8323caf82a95c4c0b25e26ea8bcca06f7
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-11 14:40:32 +0900

    feat : 데이터 로드 및 상관계수 히트맵 시각화

--- 4주차 실습/Regularization/ridge01.py
+++ 4주차 실습/Regularization/ridge01.py
@@ -11,3 +11,26 @@ from sklearn.metrics import r2_score, mean_squared_error
 
 import statsmodels.api as sm
 from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+
+df = sns.load_dataset('mpg').dropna().copy()
+# print(df.head())
+# print(df.shape) # (392,9)
+
+num_cols = df.select_dtypes(include=['int','float']).columns.tolist()
+target = 'mpg'
+num_cols = [ c for c in num_cols if c != target ]
+
+X = df[num_cols].copy()
+y = df[target].copy()
+
+print(f"Numeric features : {num_cols}")
+
+corr = X.corr(numeric_only=True).values
+plt.figure(figsize=(6,5))
+sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.3f')
+plt.xticks(range(len(num_cols)), labels=num_cols, rotation=45)
+plt.yticks(range(len(num_cols)), labels=num_cols, rotation=0)
+plt.tight_layout()
+plt.show()
+
