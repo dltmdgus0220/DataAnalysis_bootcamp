19af6db2fa7d87a06288a43f0c52675d2d52553e
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-14 16:30:00 +0900

    feat : 데이터 생성 및 분할

--- 4주차 실습/DeepLearning/pytorch03.py
+++ 4주차 실습/DeepLearning/pytorch03.py
@@ -1,4 +1,19 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import TensorDataset, Dataset, DataLoader
 import pandas as pd
+
+X = torch.linspace(0, 1, steps=100).unsqueeze(1) # 0부터 1까지 균등하게 100개 생성, unsqueeze(n):dim=n 위치에 새로운 차원 추가
+print(X.shape)
+
+y = 3*X + 1 + 0.1*torch.randn_like(X)
+
+X_train, X_test = X[:80], X[80:]
+y_train, y_test = y[:80], y[80:]
+
+train_dataset = TensorDataset(X_train, y_train)
+test_dataset = TensorDataset(X_test, y_test)
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=16, shuffle=True)
+
 
