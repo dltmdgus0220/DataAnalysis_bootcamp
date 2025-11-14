e2b94b87a0e68c4d425897a1f62f5f79257b868b
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-14 16:23:52 +0900

    fix : import pandas

--- 4주차 실습/DeepLearning/pytorch02.py
+++ 4주차 실습/DeepLearning/pytorch02.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import TensorDataset, Dataset, DataLoader
+import pandas as pd
 
 X = torch.randn(100,1) # 정규분포
 y = 3*X + 1 + 0.1*torch.randn_like(X) # randn_like:자동으로 X와 같은 shape의 정규분포를 따르는 난수생성(노이즈로써 활용)
