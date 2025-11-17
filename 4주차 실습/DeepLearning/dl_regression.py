02d0030df546a92976bda0c8c63fe2dcf438eea7
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-17 11:44:23 +0900

    feat : 선형회귀 학습 및 테스트

--- 4주차 실습/DeepLearning/dl_regression.py
+++ 4주차 실습/DeepLearning/dl_regression.py
@@ -17,3 +17,11 @@ scaler = StandardScaler()
 x_tr_scaled = scaler.fit_transform(x_tr) # 학습하고 변환까지, 학습한 결과도 저장
 x_te_scaled = scaler.transform(x_te)
 
+lin_reg = LinearRegression()
+lin_reg.fit(x_tr_scaled, y_tr)
+
+y_pred_lr = lin_reg.predict(x_te_scaled)
+rmse_lr = np.sqrt(mean_squared_error(y_te, y_pred_lr))
+
+print(f'[Linear Regression] Test RMSE: {rmse_lr:.4f}')
+
