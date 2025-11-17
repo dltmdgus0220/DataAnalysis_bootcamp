762278416c459e8c198d7e956cb83950b8bf404b
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-17 14:58:46 +0900

    feat : 데이터로드 및 분할

--- 4주차 실습/DeepLearning/dl_overfitting.py
+++ 4주차 실습/DeepLearning/dl_overfitting.py
@@ -10,3 +10,11 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 import matplotlib.pyplot as plt
 
+data = load_breast_cancer()
+X = data.data
+y = data.target
+
+x_train, x_te, y_train, y_te = train_test_split(X, y, train_size=0.2, stratify=y, random_state=42)
+x_tr, x_va, y_tr, y_va = train_test_split(x_train, y_train, train_size=0.4, stratify=y_train, random_state=42)
+
+print(x_tr.shape, x_va.shape, x_te.shape)
