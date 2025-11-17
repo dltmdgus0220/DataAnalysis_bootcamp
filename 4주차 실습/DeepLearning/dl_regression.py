01bbafde20fdb7e9bf50697cdf7c877d64744d81
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-17 11:38:07 +0900

    Create dl_regression.py

---
+++ 4주차 실습/DeepLearning/dl_regression.py
@@ -0,0 +1,9 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
