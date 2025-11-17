4160c0eed8de070ac40e6a9318366f457e262686
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-18 08:09:40 +0900

    refactor : 불필요한 라이브러리 제거

--- 4주차 실습/DeepLearning/dl_overfitting_ex.py
+++ 4주차 실습/DeepLearning/dl_overfitting_ex.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader
 
-from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
 
 import numpy as np
 import matplotlib.pyplot as plt
