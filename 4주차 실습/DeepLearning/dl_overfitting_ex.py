1f7ab6322c4e0b50248ddddf536f8c4a3122c9ac
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-18 08:09:55 +0900

    feat : 데이터 로드 및 분할

--- 4주차 실습/DeepLearning/dl_overfitting_ex.py
+++ 4주차 실습/DeepLearning/dl_overfitting_ex.py
@@ -10,3 +10,15 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+# 데이터 로드
+df = sns.load_dataset('mpg').dropna()
+
+X = df[['horsepower', 'weight']]
+y = df['mpg'].values
+
+# train/val/test 분할 (6:2:2)
+x_train, x_te, y_train, y_te = train_test_split(X, y, test_size=0.2, random_state=42)
+x_tr, x_va, y_tr, y_va = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
+
