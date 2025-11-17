bf234c34204d62372285f19e3fbee2ead1c66b9c
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-18 08:10:11 +0900

    feat : 데이터 스케일링 및 텐서 변환

--- 4주차 실습/DeepLearning/dl_overfitting_ex.py
+++ 4주차 실습/DeepLearning/dl_overfitting_ex.py
@@ -22,3 +22,21 @@ y = df['mpg'].values
 x_train, x_te, y_train, y_te = train_test_split(X, y, test_size=0.2, random_state=42)
 x_tr, x_va, y_tr, y_va = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
 
+# 스케일링
+scaler = StandardScaler()
+
+x_tr_scaled = scaler.fit_transform(x_tr)
+x_va_scaled = scaler.transform(x_va)
+x_te_scaled = scaler.transform(x_te)
+x_train_scaled = scaler.transform(x_train) # 베이스라인에 사용할 데이터
+
+# 텐서 변환
+x_tr_tensor = torch.tensor(x_tr_scaled, dtype=torch.float32)
+y_tr_tensor = torch.tensor(y_tr, dtype=torch.float32).unsqueeze(1)
+
+x_va_tensor = torch.tensor(x_va_scaled, dtype=torch.float32)
+y_va_tensor = torch.tensor(y_va, dtype=torch.float32).unsqueeze(1)
+
+x_te_tensor = torch.tensor(x_te_scaled, dtype=torch.float32)
+y_te_tensor = torch.tensor(y_te, dtype=torch.float32).unsqueeze(1)
+
