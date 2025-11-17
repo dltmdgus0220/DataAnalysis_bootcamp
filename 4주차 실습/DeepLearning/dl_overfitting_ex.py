4d9601b8eb39abc109137787f4ce44dbbeb48578
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-17 15:45:54 +0900

    Create dl_overfitting_ex.py

---
+++ 4주차 실습/DeepLearning/dl_overfitting_ex.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
