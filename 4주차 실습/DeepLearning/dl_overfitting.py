11ec657c29dedf91d83945deeb410e3f97cfe552
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-17 14:59:06 +0900

    feat : 데이터 스케일링 및 텐서변환

--- 4주차 실습/DeepLearning/dl_overfitting.py
+++ 4주차 실습/DeepLearning/dl_overfitting.py
@@ -18,3 +18,18 @@ x_train, x_te, y_train, y_te = train_test_split(X, y, train_size=0.2, stratify=y
 x_tr, x_va, y_tr, y_va = train_test_split(x_train, y_train, train_size=0.4, stratify=y_train, random_state=42)
 
 print(x_tr.shape, x_va.shape, x_te.shape)
+
+scaler = StandardScaler()
+x_tr_scaled = scaler.fit_transform(x_tr)
+x_va_scaled = scaler.transform(x_va)
+x_te_scaled = scaler.transform(x_te)
+
+x_tr_tensor = torch.tensor(x_tr_scaled, dtype=torch.float32)
+y_tr_tensor = torch.tensor(y_tr, dtype=torch.float32).unsqueeze(1)
+
+x_va_tensor = torch.tensor(x_va_scaled, dtype=torch.float32)
+y_va_tensor = torch.tensor(y_va, dtype=torch.float32).unsqueeze(1)
+
+x_te_tensor = torch.tensor(x_te_scaled, dtype=torch.float32)
+y_te_tensor = torch.tensor(y_te, dtype=torch.float32).unsqueeze(1)
+
