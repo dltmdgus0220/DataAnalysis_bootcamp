27f53d543bc9aca7e93a4332f20a0444c4324569
dltmdgus0220 <dltmdgus@khu.ac.kr>
2025-11-18 15:10:24 +0900

    feat : 토큰화 및 형태소 분석 함수 생성

--- 5주차 실습(통계기반 자연어처리)/stat_nlp02.py
+++ 5주차 실습(통계기반 자연어처리)/stat_nlp02.py
@@ -5,3 +5,25 @@ okt = Okt()
 with open('E:/데이터분석가 부트캠프/실습/DataAnalysis_bootcamp/5주차 실습(통계기반 자연어처리)/stopwords-ko.txt', encoding='utf-8') as f:
     basic_stopwords = set(w.strip() for w in f if w.strip()) # strip: 양쪽 끝 공백 혹은 파라미터로 받은 문자 제거
 
+def korean_tokenize_okt(
+        text: str,
+        tagger=okt,
+        stopwords: set | None=None, # set으로 받거나 아무것도 안받으면 None으로 받을거다
+        remove_pos=('Josa', 'Eomi', 'Punctuation', 'Suffix'),
+        min_len: int = 1,
+):
+    if stopwords is None:
+        stopwords = basic_stopwords
+
+    tokens = []
+    for word, pos in tagger.pos(text, norm=True, stem=True):
+        if pos in remove_pos: # 품사확인
+            continue
+        if word in stopwords: # 단어확인
+            continue
+        if len(word) < min_len: # 길이확인
+            continue
+        tokens.append(word)
+
+    return tokens
+
