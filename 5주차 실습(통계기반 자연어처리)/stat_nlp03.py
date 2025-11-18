d30d01944326bebc4bf6cbea7245c0ed358d02ac
dltmdgus0220 <dltmdgus@khu.ac.kr>
2025-11-18 15:11:08 +0900

    Create stat_nlp03.py

---
+++ 5주차 실습(통계기반 자연어처리)/stat_nlp03.py
@@ -0,0 +1,10 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+
+# nltk.download("stopwords") # 영어 불용어 리스트
+# nltk.download("punkt") # 기본 토크나이저
+# nltk.download("punkt_tab") # 최신 NLTK에서 추가로 요구
+# nltk.download("averaged_perceptron_tagger_eng") # 품사 태거
+
