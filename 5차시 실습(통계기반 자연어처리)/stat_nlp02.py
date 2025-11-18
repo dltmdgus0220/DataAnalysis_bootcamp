4042f62ab3b7dff1d66ba301aea1967783272559
dltmdgus0220 <dltmdgus@khu.ac.kr>
2025-11-18 16:07:48 +0900

    refactor : 디렉토리 변경

--- 5차시 실습(통계기반 자연어처리)/stat_nlp02.py
+++ 5차시 실습(통계기반 자연어처리)/stat_nlp02.py
@@ -2,10 +2,8 @@ from konlpy.tag import Okt
 
 okt = Okt()
 
-with open('E:/데이터분석가 부트캠프/실습/DataAnalysis_bootcamp/5주차 실습(통계기반 자연어처리)/stopwords-ko.txt', encoding='utf-8') as f:
-    for w in f:
-        print(w.strip())
-    basic_stopwords = set(w.strip() for w in f if w.strip()) # strip: 양쪽 끝 공백 혹은 파라미터로 받은 문자 제거
+with open('E:/데이터분석가 부트캠프/실습/DataAnalysis_bootcamp/5차시 실습(통계기반 자연어처리)/stopwords-ko.txt', encoding='utf-8') as f:
+    basic_stopwords = set(w.strip() for w in f if w.strip())
 
 def korean_tokenize_okt(
         text: str,
