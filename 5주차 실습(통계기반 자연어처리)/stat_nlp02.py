f947b1951fbbce3acab37e9696576780b0e92e93
dltmdgus0220 <dltmdgus@khu.ac.kr>
2025-11-18 15:09:25 +0900

    feat : 불용어 텍스트 파일 불러오기

--- 5주차 실습(통계기반 자연어처리)/stat_nlp02.py
+++ 5주차 실습(통계기반 자연어처리)/stat_nlp02.py
@@ -1,1 +1,7 @@
 from konlpy.tag import Okt
+
+okt = Okt()
+
+with open('E:/데이터분석가 부트캠프/실습/DataAnalysis_bootcamp/5주차 실습(통계기반 자연어처리)/stopwords-ko.txt', encoding='utf-8') as f:
+    basic_stopwords = set(w.strip() for w in f if w.strip()) # strip: 양쪽 끝 공백 혹은 파라미터로 받은 문자 제거
+
