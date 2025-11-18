b1d5caef657e3408077d7e04737311f6da83e5a5
dltmdgus0220 <dltmdgus@khu.ac.kr>
2025-11-18 15:29:11 +0900

    feat : nltk를 활용한 불용어 처리

--- 5주차 실습(통계기반 자연어처리)/stat_nlp03.py
+++ 5주차 실습(통계기반 자연어처리)/stat_nlp03.py
@@ -8,3 +8,20 @@ from nltk import pos_tag
 # nltk.download("punkt_tab") # 최신 NLTK에서 추가로 요구
 # nltk.download("averaged_perceptron_tagger_eng") # 품사 태거
 
+stop_words = set(stopwords.words("english"))
+# print(list(stop_words)[:30]) 
+print(type(stop_words), len(stop_words))
+
+example_sentence = "This is an example showing off stop word filtration."
+
+words = word_tokenize(example_sentence) # 토큰화
+print(words)
+
+filtered_sentence = [ word for word in words if word.lower() not in stop_words ]
+print('원문:', example_sentence)
+print('불용어 제거 후:', filtered_sentence)
+
+tags = pos_tag(filtered_sentence)
+print(tags)
+# [('example', 'NN'), ('showing', 'VBG'), ('stop', 'JJ'), ('word', 'NN'), ('filtration', 'NN'), ('.', '.')]
+# NN:명사단수, VBG:동명사/현재분사, JJ:형용사
