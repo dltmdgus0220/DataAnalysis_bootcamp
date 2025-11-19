156cf3665f08ec9fc736370a28f5cb36240db4a7
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-19 14:34:10 +0900

    style : 줄바꿈 추가

--- 5차시 실습(통계기반 자연어처리)/bag_of_word05.py
+++ 5차시 실습(통계기반 자연어처리)/bag_of_word05.py
@@ -7,6 +7,7 @@ docs = [ "오늘 날씨가 좋아서 산책을 했다",
         "점심에 맛있는 파스타를 먹었다",
         "저녁에 산책하면서 음악을 들었다"
         ]
+
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(docs)
 print(vectorizer.get_feature_names_out())
