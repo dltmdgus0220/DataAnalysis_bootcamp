62c1a19d91eb1263dfb45b75dbf798acf1771f31
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-19 16:25:55 +0900

    feat : 데이터 층화추출 및 train/test 분할

--- 5차시 실습(통계기반 자연어처리)/bag_of_word07.py
+++ 5차시 실습(통계기반 자연어처리)/bag_of_word07.py
@@ -16,3 +16,10 @@ df = pd.read_csv('5차시 실습(통계기반 자연어처리)/movie_reviews.csv
 # print(df)
 
 
+# 2. train/test 분할
+df_sample, _ = train_test_split(df, train_size=10000, random_state=42, stratify=df['label']) # shuffle=True는 기본값이라 생략
+text, label = df_sample['document'], df_sample['label']
+x_tr, x_te, y_tr, y_te = train_test_split(text, label, test_size=0.2, random_state=42, stratify=label)
+# print(len(x_tr), len(x_te))
+
+
