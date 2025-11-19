ac1375398608bee1b513a534a187c8a2dd1064b5
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-19 11:49:33 +0900

    feat : unigram

--- 5차시 실습(통계기반 자연어처리)/bag_of_word03.py
+++ 5차시 실습(통계기반 자연어처리)/bag_of_word03.py
@@ -5,3 +5,11 @@ docs = [
     '오늘 기분 정말 좋다',
     '오늘은 기분이 좋지 않다'
 ]
+
+# unigram (단어 한개씩)
+cv_uni = CountVectorizer(ngram_range=(1, 1))
+X_uni = cv_uni.fit_transform(docs)
+print("unigram vocab:", cv_uni.get_feature_names_out())
+print(X_uni.toarray())
+print()
+
