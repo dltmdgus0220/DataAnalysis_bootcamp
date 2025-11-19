a25c812d4d5cf8424983a8df06a79d939772b1f5
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-19 13:34:35 +0900

    feat : train/test 분할

--- 5차시 실습(통계기반 자연어처리)/bag_of_word04.py
+++ 5차시 실습(통계기반 자연어처리)/bag_of_word04.py
@@ -16,3 +16,18 @@ print("vocab:", tfidf.get_feature_names_out())
 print("shape:", X_tfidf.shape)
 print(X_tfidf.toarray().round(3)) # 단어별 중요도 출력
 # tfidf가 높으려면 현 문장에서 자주 나오면서 동시에 다른 문장에서는 많이 안나와야 됨.
+
+
+texts = [
+    "이 영화 정말 재밌어요 최고에요",
+    "별로 재미없고 지루했어요",
+    "배우 연기도 좋고 감동적이었어요",
+    "스토리가 엉망이고 시간 아까웠어요",
+    "완전 최고 강추합니다",
+    "돈 아깝고 다시는 보고 싶지 않아요"
+]
+
+labels = [1,0,1,0,1,0]
+
+# train/test 분할s
+x_tr, x_te, y_tr, y_te = train_test_split(texts, labels, test_size=0.3, random_state=42)
