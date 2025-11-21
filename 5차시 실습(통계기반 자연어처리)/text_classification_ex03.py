13c6e468e02fa489b7f40315392a40547d068cb4
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-21 17:45:55 +0900

    feat : 데이터로드(클래스별 600개씩)

--- 5차시 실습(통계기반 자연어처리)/text_classification_ex03.py
+++ 5차시 실습(통계기반 자연어처리)/text_classification_ex03.py
@@ -13,3 +13,34 @@ from sklearn.metrics import classification_report, f1_score
 from sklearn.utils.class_weight import compute_sample_weight
 
 okt = Okt()
+
+rows = [] # 아예 딕셔너리 형태로 넣고 나중에 데이터프레임으로 바꿔버리기
+i = 1
+class_count = {} # 클래스별 데이터 수를 저장할 딕셔너리
+while True: # 클래스별 데이터 수가 500개가 될때까지 반복
+    if len(class_count) > 0 and all(v >= 600 for v in class_count.values()):
+        print("모든 클래스 목표 개수 도달")
+        break
+    
+    path = os.path.join('5차시 실습(통계기반 자연어처리)/1-1. 여성의류', f'1-1.여성의류({i}).json')
+
+    if not os.path.isfile(path):
+        print(f"더 이상 읽을 파일 없음.")
+        break
+
+    if os.path.isfile(path):
+        with open(path, encoding='utf-8') as f:
+            data = json.load(f) # 100개씩 가지고 있음
+            df_tmp = pd.DataFrame(data)[['RawText','GeneralPolarity']].dropna()
+            for _, row in df_tmp.iterrows(): # 행으로 읽어옴 ,(인덱스, ['RawText','GeneralPolarity']:series) 형태의 튜플. 따라서 뒤에꺼만 필요함
+                c = row['GeneralPolarity']
+                count = class_count.get(c,0)
+                if count < 500: # 500개가 되지 않으면
+                    rows.append({
+                        'RawText':row['RawText'],
+                        'GeneralPolarity':row['GeneralPolarity']
+                    })
+                    class_count[c] = count + 1 # 없으면 생성, 있으면 업데이트
+
+print(len(rows))
+df = pd.DataFrame(rows)
