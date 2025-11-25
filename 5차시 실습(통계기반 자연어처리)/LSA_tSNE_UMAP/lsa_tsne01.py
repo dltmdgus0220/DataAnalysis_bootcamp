ea267eacc942e632a51e92dcca40365d3376dfe9
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-25 15:26:20 +0900

    feat : 정규화 함수 생성

--- 5차시 실습(통계기반 자연어처리)/LSA_tSNE_UMAP/lsa_tsne01.py
+++ 5차시 실습(통계기반 자연어처리)/LSA_tSNE_UMAP/lsa_tsne01.py
@@ -13,3 +13,9 @@ if platform.system() == "Windows":
     plt.rc('font', family="Malgun Gothic")
 plt.rcParams['axes.unicode_minus']=False
 
+
+def simple_korea_clean(text):
+    text = re.sub(r'[^가-힣0-9\s]', ' ', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+
