4337cb40713b1044092eb780426e004818f0a302
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-25 15:25:04 +0900

    feat : 데이터로드

--- 5차시 실습(통계기반 자연어처리)/LSA_tSNE_UMAP/tsne01.py
+++ 5차시 실습(통계기반 자연어처리)/LSA_tSNE_UMAP/tsne01.py
@@ -2,3 +2,8 @@ from sklearn.datasets import load_digits
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
+# 데이터 로드 (손글씨 숫자 0~9)
+digits = load_digits()
+X = digits.data      # (n_samples, 64) 8x8 이미지 픽셀
+y = digits.target    # 라벨(0~9)
+
