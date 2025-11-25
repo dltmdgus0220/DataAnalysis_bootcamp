f5ba5cedbe98e41a0b7f8f42b3369e200bc65764
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-25 15:25:44 +0900

    feat : tsne로 차원축소

--- 5차시 실습(통계기반 자연어처리)/LSA_tSNE_UMAP/tsne01.py
+++ 5차시 실습(통계기반 자연어처리)/LSA_tSNE_UMAP/tsne01.py
@@ -7,3 +7,11 @@ digits = load_digits()
 X = digits.data      # (n_samples, 64) 8x8 이미지 픽셀
 y = digits.target    # 라벨(0~9)
 
+# t-SNE로 2차원 축소
+tsne = TSNE(
+    n_components=2, perplexity=30,
+    learning_rate=200, random_state=42
+)
+
+X_2d = tsne.fit_transform(X)
+
