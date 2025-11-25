6cb787ea7a92ee93fb1e69c50ef5fc59b4b944b4
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-25 17:35:02 +0900

    feat : 텍스트 전처리 함수 생성

--- 5차시 실습(통계기반 자연어처리)/LSA_tSNE_UMAP/isa_ex01.py
+++ 5차시 실습(통계기반 자연어처리)/LSA_tSNE_UMAP/isa_ex01.py
@@ -1,9 +1,9 @@
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
+from konlpy.tag import Okt
 
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 import umap.umap_ as umap
@@ -12,3 +12,26 @@ import platform
 if platform.system() == "Windows":
     plt.rc('font', family="Malgun Gothic")
 plt.rcParams['axes.unicode_minus']=False
+
+
+okt = Okt()
+stopwords = ['하다', '입다', '이다', '같다', '좀', 
+             '조금', '있다', '개', '이다', '요',
+             '성은', '듭니', '해', '해도', '이렇다', '나다']
+
+
+def preprocess(text):
+    text = re.sub(r'[^가-힣0-9\s]', ' ', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    morphs_text = okt.pos(text, norm=True, stem=True)
+    content_words = []
+    for word, pos in morphs_text:
+        if pos in ["Noun", "Verb", "Adjective"]:
+            if word not in stopwords: # and len(word) > 1:
+                content_words.append(word)
+
+    return ' '.join(content_words)
+
+# 이후에 HDBSCAN이나 KMeans등으로 군집을 생성하고 군집별 데이터를 확인하는 작업이 필요함.
+
