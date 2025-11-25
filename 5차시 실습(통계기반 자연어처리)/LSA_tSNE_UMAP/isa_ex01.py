6eabb5cf583871dd6a2b491a5d8902f49a94ebae
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-25 16:20:57 +0900

    Create isa_ex01.py

---
+++ 5차시 실습(통계기반 자연어처리)/LSA_tSNE_UMAP/isa_ex01.py
@@ -0,0 +1,14 @@
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+import umap.umap_ as umap
+
+import platform
+if platform.system() == "Windows":
+    plt.rc('font', family="Malgun Gothic")
+plt.rcParams['axes.unicode_minus']=False
