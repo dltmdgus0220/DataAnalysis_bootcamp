e57526b4d341ec46edf13add23d4db5c39472b1b
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-25 15:22:28 +0900

    feat : 데이터 생성

--- 5차시 실습(통계기반 자연어처리)/LSA_tSNE_UMAP/lsa01.py
+++ 5차시 실습(통계기반 자연어처리)/LSA_tSNE_UMAP/lsa01.py
@@ -10,3 +10,13 @@ if platform.system() == "Windows":
     plt.rc('font', family="Malgun Gothic")
 plt.rcParams['axes.unicode_minus']=False
 
+
+docs = [
+"이 브랜드 배송이 너무 빨라서 좋았어요",
+"품질은 괜찮은데 배송이 너무 느려요",
+"가격이 저렴해서 가성비가 좋아요",
+"브랜드 이미지가 세련되고 품질도 좋아요",
+"배송도 빠르고 포장도 깔끔해서 만족합니다",
+"가격은 비싼 편인데 품질이 좋아요",
+]
+
