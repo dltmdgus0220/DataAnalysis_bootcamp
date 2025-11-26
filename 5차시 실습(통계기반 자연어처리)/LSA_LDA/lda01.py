e064d9b5f7104e2573b53c5a1449a1ce821232bd
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-26 14:41:23 +0900

    feat : 토픽별 상위 단어 출력 함수 생성

--- 5차시 실습(통계기반 자연어처리)/LSA_LDA/lda01.py
+++ 5차시 실습(통계기반 자연어처리)/LSA_LDA/lda01.py
@@ -26,3 +26,11 @@ doc_topic = lda.fit_transform(X)
 # print(doc_topic)
 # print(doc_topic.sum(axis=1))
 # print(lda.components_) # 각 토픽의 단어 분포, 크면 클수록 해당 토픽에서 그 단어가 등장할 가능성이 높다
+
+# 토픽별 상위 단어 출력 함수
+def print_topics(model, feature_names, n_top_word=5):
+    for topic_idx, topic in enumerate(model.components_):
+        top_indices = topic.argsort()[::-1][:n_top_word]
+        top_words = [feature_names[i] for i in top_indices]
+        print(f'Topic {topic_idx} : {", ".join(top_words)}')
+
