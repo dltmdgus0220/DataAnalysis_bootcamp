82cf280d29092bbc6bf0d2af6c2b9774821c4f3b
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-26 14:16:13 +0900

    Create lda01.py

---
+++ 5차시 실습(통계기반 자연어처리)/LSA_LDA/lda01.py
@@ -0,0 +1,12 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+import numpy as np
+
+docs = [
+    "배송이 빠르고 포장이 깔끔해서 좋았어요",
+    "배송이 느리고 박스가 찢어져 와서 별로였어요",
+    "가격이 생각보다 저렴해서 가성비가 좋아요",
+    "가격이 비싼 편인데 품질이 좋아서 만족해요",
+    "품질이 안 좋아서 실망했어요",
+    "배송은 빠르지만 품질이 너무 안 좋아요",
+]
