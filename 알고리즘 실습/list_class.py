32a93c63926a6850453c9d9b313519c8f7c50fa7
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-27 11:39:42 +0900

    feat : 데이터 제거 함수 추가

--- 알고리즘 실습/list_class.py
+++ 알고리즘 실습/list_class.py
@@ -37,3 +37,19 @@ class LinkedList:
         new_node.next = prev.next
         prev.next = new_node
             
+    # 제거
+    def delete(self, index):
+        if self.head is None: # 비어있는지
+            raise IndexError('Index Out of Range')
+        if index >= self.length(): # 인덱스 범위 벗어나는지
+            raise IndexError('Index Out of Range')
+        
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        prev = self.head
+        for _ in range(index-1):
+            prev = prev.next
+        prev.next = prev.next.next
+
