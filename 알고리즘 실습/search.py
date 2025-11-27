c51f3e49c918d18f09f77a00f47cd08c12a0835a
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-11-27 11:41:02 +0900

    feat : 이진탐색 함수 생성

--- 알고리즘 실습/search.py
+++ 알고리즘 실습/search.py
@@ -5,6 +5,24 @@ def linear_search(data_list, target):
             return i
     return -1
 
+# 이진 탐색 O(logn), 정렬되어 있는 리스트에서 빠르게 찾을 수 있음
+def binary_search(data_list, target):
+    start, end = 0, len(data_list)-1
+
+    while start <= end:
+        mid = (start+end)//2
+        # print('mid:',mid)
+        if data_list[mid] == target:
+            return mid
+        if data_list[mid] > target:
+            end = mid-1
+        else:
+            start = mid+1
+    return -1
+
+
+
+
 target_list = [5, 2, 8, 1, 9, 4]
 print(target_list)
 for i in range(8, 11):
