03ffd99f2dc1569ede7624b739ac77ac77da0b2f
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-12-01 13:26:45 +0900

    feat : elbow 함수 생성

--- 5차시 실습(통계기반 자연어처리)/module/optimize_kmeans.py
+++ 5차시 실습(통계기반 자연어처리)/module/optimize_kmeans.py
@@ -2,3 +2,24 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
 
+
+def elbow(vector, start:int=1, end:int=10, random_state:int=42):
+    inertias = []
+
+    for k in range(start, end+1):
+        kmeans = KMeans(
+            n_clusters=k,
+            random_state=42,
+            n_init="auto"
+        )
+        kmeans.fit(vector)
+        inertias.append(kmeans.inertia_)
+        
+    plt.plot(range(start, end+1), inertias, marker="o")
+    plt.xlabel("k (클러스터 개수)")
+    plt.ylabel("Inertia (SSE)")
+    plt.title("Elbow Method")
+    plt.show()
+
+    return inertias
+
