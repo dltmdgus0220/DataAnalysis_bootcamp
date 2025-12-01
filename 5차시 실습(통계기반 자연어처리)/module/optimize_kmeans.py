370f0e6f9a743b3fee08dbf744dbfac26d8931c6
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-12-01 13:27:01 +0900

    feat : silhouette 함수 생성

--- 5차시 실습(통계기반 자연어처리)/module/optimize_kmeans.py
+++ 5차시 실습(통계기반 자연어처리)/module/optimize_kmeans.py
@@ -23,3 +23,30 @@ def elbow(vector, start:int=1, end:int=10, random_state:int=42):
 
     return inertias
 
+
+def silhouette(vector, start:int=2, end:int=10, random_state:int=42): # X:UMAP 기반 좌표, df
+    sil_scores = []
+
+    for k in range(start, end+1):
+        kmeans = KMeans(
+        n_clusters=k,
+        random_state=random_state,
+        n_init="auto"
+        )
+        labels = kmeans.fit_predict(vector)
+        # a(i): 자기 군집 안에서의 평균 거리
+        # b(i): 가장 가까운 다른 군집과의 평균 거리
+        # (b - a) / max(a, b)
+        score = silhouette_score(vector, labels)  # 전체 평균 실루엣 점수
+        sil_scores.append(score)
+        print(f"k={k}, silhouette_score={score:.4f}")
+
+    plt.plot(range(start, end+1), sil_scores, marker="o")
+    plt.xlabel("k (클러스터 개수)")
+    plt.ylabel("Silhouette Score")
+    plt.title("Silhouette Method")
+    plt.show()
+
+    return sil_scores
+
+
