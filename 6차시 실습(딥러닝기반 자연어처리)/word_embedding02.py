37d0af48726274e14debd0246a0be0ea9a1022aa
dltmdgus0220 <dltmdgus@khu.ac.kr>
2025-12-02 14:58:26 +0900

    data : 간단한 말뭉치 데이터 추가

--- 6차시 실습(딥러닝기반 자연어처리)/word_embedding02.py
+++ 6차시 실습(딥러닝기반 자연어처리)/word_embedding02.py
@@ -2,3 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# 1) 간단한 말뭉치
+sentences = [
+    '이 영화 정말 최고였어요.',
+    '배우 연기가 최고입니다.',
+    '내용이 지루하고 별로였어요.',
+    '스토리가 지루하지만 배우는 좋았어요.'
+]
+
