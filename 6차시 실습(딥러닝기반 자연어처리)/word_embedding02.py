cbdc80b09380143e1290fa8b21fe661a62db7d60
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-12-03 11:14:13 +0900

    feat : 임베딩 추가

--- 6차시 실습(딥러닝기반 자연어처리)/word_embedding02.py
+++ 6차시 실습(딥러닝기반 자연어처리)/word_embedding02.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from collections import Counter
 
-# 1) 간단한 말뭉치
+# 1. 간단한 말뭉치
 sentences = [
     '이 영화 정말 최고였어요.',
     '배우 연기가 최고입니다.',
@@ -11,7 +11,7 @@ sentences = [
     '스토리가 지루하지만 배우는 좋았어요.'
 ]
 
-# 2) vocab 만들기
+# 2. vocab 만들기
 tokens = [s.split() for s in sentences]
 # print(tokens)
 
@@ -24,3 +24,11 @@ vocab = {'<PAD>': 0, '<UNK>': 1}
 for word, _ in counter.most_common():
     vocab[word] = len(vocab)
 print(vocab)
+
+
+# 3. 임베딩
+vocab_size = len(vocab)
+embed_dim = 8
+
+embed = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_dim)
+
