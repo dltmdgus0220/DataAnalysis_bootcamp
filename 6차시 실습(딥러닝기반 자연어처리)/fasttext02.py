eec4f3ed99ca04e3deeb5aaef684c9f5ae870815
dltmdgus0220 <dltmdgus@khu.ac.kr>
2025-12-04 14:37:42 +0900

    feat : 데이터로드 및 간단한 EDA

--- 6차시 실습(딥러닝기반 자연어처리)/fasttext02.py
+++ 6차시 실습(딥러닝기반 자연어처리)/fasttext02.py
@@ -8,3 +8,11 @@ from torch.nn.utils.rnn import pad_sequence
 from sklearn.model_selection import train_test_split
 from gensim.models import FastText
 
+df = pd.read_csv('5차시 실습(통계기반 자연어처리)/movie_reviews.csv', encoding='utf-8').dropna()
+df = df.reset_index(drop=True) # 결측치 제거 후 인덱스 재배열
+# print(df.shape) # (199992, 3)
+# print(df.head())
+# print(df.columns) # id, document, label
+# print(df.info())
+# print(df['label'].value_counts()) # 1(긍정):99996, 0(부정):99996
+
