3bbb1ac289b92fa054e23cf3620a8b827efd5364
dltmdgus0220 <dltmdgus@khu.ac.kr>
2025-12-04 14:37:26 +0900

    Create fasttext02.py

---
+++ 6차시 실습(딥러닝기반 자연어처리)/fasttext02.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
+from sklearn.model_selection import train_test_split
+from gensim.models import FastText
+
