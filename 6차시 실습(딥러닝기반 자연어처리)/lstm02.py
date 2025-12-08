c4976b1e0cb885c5551730db088abffaa062361c
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-12-08 10:10:18 +0900

    data : 텍스트 데이터 생성

--- 6차시 실습(딥러닝기반 자연어처리)/lstm02.py
+++ 6차시 실습(딥러닝기반 자연어처리)/lstm02.py
@@ -6,3 +6,14 @@ from torch.utils.data import Dataset, DataLoader, TensorDataset
 import torch.nn.functional as F
 from tqdm import tqdm
 
+
+raw_texts = [
+    "영화가 정말 재미있고 감동적이었어요",
+    "스토리가 지루하고 시간 낭비였어요",
+    "배우 연기가 훌륭하고 음악도 좋았어요",
+    "내용이 별로고 전개가 너무 느렸어요",
+    "정말 최고의 영화였어요 또 보고 싶어요",
+    "연출이 엉성하고 집중이 안 됐어요",
+]
+raw_labels = [1, 0, 1, 0, 1, 0]
+
