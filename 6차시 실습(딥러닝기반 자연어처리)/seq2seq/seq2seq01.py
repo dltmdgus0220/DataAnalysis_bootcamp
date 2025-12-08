5920bbf14b382705a92eaac96288d3430ba17434
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-12-08 09:48:49 +0900

    feat : 토큰 생성

--- 6차시 실습(딥러닝기반 자연어처리)/seq2seq/seq2seq01.py
+++ 6차시 실습(딥러닝기반 자연어처리)/seq2seq/seq2seq01.py
@@ -6,3 +6,8 @@ from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from tqdm import tqdm
 
+chars = list('abcdefghijklmnopqrstuvwxyz')
+PAD_TOKEN = '<pad>'
+SOS_TOKEN = '<sos>'
+EOS_TOKEN = '<eos>'
+
