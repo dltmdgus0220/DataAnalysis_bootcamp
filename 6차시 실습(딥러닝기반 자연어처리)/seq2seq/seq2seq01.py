9aca568f06777087ff9d37c15ad66056d54a22ac
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-12-08 09:48:37 +0900

    Create seq2seq01.py

---
+++ 6차시 실습(딥러닝기반 자연어처리)/seq2seq/seq2seq01.py
@@ -0,0 +1,8 @@
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
+from tqdm import tqdm
+
