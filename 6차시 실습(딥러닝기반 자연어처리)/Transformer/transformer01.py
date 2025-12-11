86fe925f43af2125ec0c548d990c4056a5afef83
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-12-11 15:24:24 +0900

    feat : 상수 선언

--- 6차시 실습(딥러닝기반 자연어처리)/Transformer/transformer01.py
+++ 6차시 실습(딥러닝기반 자연어처리)/Transformer/transformer01.py
@@ -7,3 +7,28 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
 
+
+# 상수 선언
+NUM_LETTERS = 8
+PAD_IDX = 0
+SOS_IDX = 1
+EOS_IDX = 2
+VOCAB_TOKENS = ['<pad>','<sos>','<eos>'] + [chr(ord('a')+i) for i in range(NUM_LETTERS)]
+VOCAB_SIZE = len(VOCAB_TOKENS)
+
+MIN_SEQ_LEN = 3
+MAX_SEQ_LEN = 8
+NUM_TRAIN_SAMPLES = 20000
+NUM_VALID_SAMPLES = 2000
+
+D_MODEL = 16
+NHEAD = 4
+NUM_ENCODER_LAYERS = 2
+NUM_DECODER_LAYERS = 2
+DIM_FEEDFORWARD = 32
+
+BATCH_SIZE = 32
+NUM_EPOCHS = 15
+LEARNING_RATE = 1e-3
+DROPOUT = 0.2
+
