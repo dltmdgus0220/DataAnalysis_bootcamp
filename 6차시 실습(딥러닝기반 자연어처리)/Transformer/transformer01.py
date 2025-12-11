37ce7c04a250d0d439f9d601d800d40aa29a1b58
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2025-12-11 15:49:07 +0900

    docs : div_term shape 주석 추가

--- 6차시 실습(딥러닝기반 자연어처리)/Transformer/transformer01.py
+++ 6차시 실습(딥러닝기반 자연어처리)/Transformer/transformer01.py
@@ -133,7 +133,7 @@ class PositionalEncoding(nn.Module):
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float32).unsqueeze(1) # (max_len, 1)
 
-        div_term = torch.exp(torch.arange(0,d_model,2).float() * (-math.log(10000.0) / d_model)) # 스케일링을 위한 값
+        div_term = torch.exp(torch.arange(0,d_model,2).float() * (-math.log(10000.0) / d_model)) # 스케일링을 위한 값, (d_model//2,)
 
         pe[:, 0::2] = torch.sin(position * div_term) # 짝수 차원
         pe[:, 1::2] = torch.cos(position * div_term) # 홀수 차원
